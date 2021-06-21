c9779ec26281336d24071f226a2ff1b92ea3e171
Vikramjeet Ahlawat <vikram210191@gmail.com>
2021-06-21 14:49:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
 let billArray = [];
-var bill = 0;
-function addPrice(a){
+let bill = 0;
+let item = [];
+let itemName;
+localStorage.clear();
+function addPrice(a, b){
     console.log(billArray.push(a));
-
+    setTimeout(genBill,200);
+    itemName = document.getElementsByClassName('itemBill')[b].value;
+    item.push([itemName, a]);
+    localStorage.setItem('itemsJson',JSON.stringify(item));
+    console.log(item);
 }
 
 function genBill(){
