109b9c0ba34cd5e34864963c9762de8800a5cb16
Vikramjeet Ahlawat <vikram210191@gmail.com>
2021-06-22 12:22:52 +0530

    Add files via upload

--- bill.js
+++ bill.js
@@ -5,22 +5,22 @@ console.log(items);
 let newArr;
 for(var i=0; i<items.length;i++){
 
-    if (i==0) {
+    if (i=0) {
         items[i] = newArr;
         document.getElementsByClassName('iName-0').innerHTML= newArr[0];
         document.getElementsByClassName('iValue-0').innerHTML= newArr[1];
     }
-    else if (i==1) {
+    else if (i=1) {
         items[i] = newArr;
         document.getElementsByClassName('iName-1').innerHTML= newArr[0];
         document.getElementsByClassName('iValue-1').innerHTML= newArr[1];
     }
-    else if (i==2) {
+    else if (i=2) {
         items[i] = newArr;
         document.getElementsByClassName('iName-2').innerHTML= newArr[0];
         document.getElementsByClassName('iValue-2').innerHTML= newArr[1];
     }
-    else if (i==3) {
+    else if (i=3) {
         items[i] = newArr;
         document.getElementsByClassName('iName-3').innerHTML= newArr[0];
         document.getElementsByClassName('iValue-3').innerHTML= newArr[1];
