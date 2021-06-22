d8f5a3df2c71a9204b2e43f79c72826faf486b05
Vikramjeet Ahlawat <vikram210191@gmail.com>
2021-06-22 12:31:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ let item = [];
 let itemName;
 
 
-localStorage.clear();             // clear local storage
+localStorage.clear();
 
 
 function addPrice(a, b){
     console.log(billArray.push(a));
     setTimeout(genBill,200);
-    itemName = document.getElementsByClassName('itemBill')[b].value;          // getting item and its value
+    itemName = document.getElementsByClassName('itemBill')[b].value;
     item.push([itemName, a]);
-    localStorage.setItem('itemsJson', JSON.stringify(item));
+    localStorage.setItem('itemsJson',JSON.stringify(item));
     console.log(item);
 }
 
 function genBill(){
-for(i=0;i<billArray.length;i++){                          // calculating bill
+for(i=0;i<billArray.length;i++){
     bill = bill + billArray[i];
 }
 
 
 billArray=[];
-document.querySelector('.cart-total-price').innerHTML=" ₹"+ bill;  // generating bill
+document.querySelector('.cart-total-price').innerHTML=" ₹"+ bill;
 }
